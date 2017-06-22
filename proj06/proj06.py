441a47303d999d153a40d662c563f4dac7348c04
Ya Boi <kstingley10@gmail.com>
2017-06-22 15:40:35 -0500

    Add files via upload

--- proj06/proj06.py
+++ proj06/proj06.py
@@ -15,7 +15,7 @@ WORDLIST_FILENAME = "words.txt"
 def load_words():
     """
     Returns a list of valid words. Words are strings of lowercase letters.
-    
+
     Depending on the size of the word list, this function may
     take a while to finish.
     """
@@ -40,9 +40,73 @@ def choose_word(wordlist):
 # end of helper code
 # -----------------------------------
 
-# actually load the dictionary of words and point to it with 
+# actually load the dictionary of words and point to it with
 # the wordlist variable so that it can be accessed from anywhere
 # in the program
 wordlist = load_words()
 
 # your code begins here!
+print ("Welcome to Hangman! Be ready to lose...")
+
+listwrong=[]
+
+#print wordlist
+chosenword=choose_word(wordlist)
+print chosenword
+guesses = 1
+
+emptylist = []
+for letters in chosenword:
+    emptylist.append("_")
+print emptylist
+
+chosenletter = raw_input("Type in a letter please! And after 8 trials of defeat, you shall perish.")
+print [chosenletter]
+
+while guesses > 0:
+    if chosenletter not in chosenword:
+        print ("that is incorrect")
+        guesses = guesses-1
+        listwrong.append(chosenletter)
+        print listwrong
+
+if letters == chosenword:
+    print "Correct, you are still terrible, but just keep trying... hahahaha!"
+
+if chosenletter == chosenword:
+    print ("You must have cheated to accomplish beating me, but u have won :(")
+
+
+# for letters in chosenword:
+#     if letters== chosenletter:
+#         print letters
+
+
+#         while chosenword < chosenletter:
+#
+#             print fools_game
+# for guesses in range(1,9):
+#     if chosenletter > chosenword:
+#         print True
+#
+#     else:
+#         print guesses
+#
+# for letters in chosenword:
+#     print [""]
+#
+# #
+# for letters in chosenword:
+#
+#         if letters == chosenletter:
+#
+#             print letters
+#
+#     else:
+#         print [""]
+
+# for guesses in chosenword:
+#     if chosenword < letters:
+#         print fools_game
+
+
