db87e018d718c415db3267dfea30a3cd919a9191
Ya Boi <kstingley10@gmail.com>
2017-06-26 15:41:59 -0500

    Add files via upload

--- proj08/RSS_Feed.py
+++ proj08/RSS_Feed.py
@@ -1,5 +1,5 @@
-# Name:
-# Date
+# Name:Damian and Bonnie
+# Date:June 26th, 2017
 
 import feedparser
 import string
@@ -47,6 +47,27 @@ def process(url):
 
 # TODO: NewsStory
 
+
+class NewsStory(object):
+    def __init__(self, guid, title, subject, summary, link):
+        self.guid = guid
+        self.title = title
+        self.subject = subject
+        self.summary = summary
+        self.link = link
+    def get_guid(self):
+        return self.guid
+    def get_title(self):
+        return self.title
+    def get_subject(self):
+        return self.subject
+    def get_summary(self):
+        return self.summary
+    def get_link(self):
+        return self.link
+
+
+
 #======================
 # Part 2
 # Triggers
@@ -60,12 +81,25 @@ class Trigger(object):
         """
         raise NotImplementedError
 
+
 # Whole Word Triggers
 # Problems 2-5
 
 # TODO: WordTrigger
+class wordtrigger(str):
+    def wordtrigger(self, story):
+        if wordtrigger in story:
+            return True
+        else:
+            return False
+
+
+
+
 
 # TODO: TitleTrigger
+class titletrigger(str:
+    )
 # TODO: SubjectTrigger
 # TODO: SummaryTrigger
 
